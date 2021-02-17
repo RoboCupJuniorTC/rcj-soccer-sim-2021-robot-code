e6c37ca0ee5fef9d6d91bbff328c0165a64cc175
David Schwarz <rcj-soccer-sim-feb-21@davidschwarz.info>
2021-02-17 03:38:24 +0100

    038 now working

--- 038/robot3/robot3.py
+++ 038/robot3/robot3.py
@@ -1,3 +1,8 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path('.').absolute().parent))
+sys.path.append('/app/controllers')
+
 # rcj_soccer_player controller - ROBOT B3
 
 ###### REQUIRED in order to import files from B1 controller
@@ -7,7 +12,7 @@ sys.path.append(str(Path('.').absolute().parent))
 # You can now import scripts that you put into the folder with your
 # robot B1 controller
 
-from bishops_knights_d1 import rcj_soccer_robot, utils
+from team_038_libraries.robot1 import rcj_soccer_robot, utils
 ######
 
 # Feel free to import built-in libraries
